e4e4adc34c3a0467ec940bdaa7f68fb607de3e3c
RANDY\randy <randydangg@gmail.com>
2019-02-02 20:29:10 -0500

    Switched communication method, removed Assignment_1.1

--- Assignment_1/src/NotesClient.java
+++ Assignment_1/src/NotesClient.java
@@ -1,29 +1,48 @@
 import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class NotesClient {
-	private static int PORT = 9898; // Port number will be entered by client
-
+	private static int PORT = 9890;	//appropriate port#? 
+	
 	public static void main(String[] args) throws Exception {
-		// System.out.println("Enter the IP address of a machine running the
-		// capitalize server:");
-		String serverAddress = "192.168.232.1";// new
-												// Scanner(System.in).nextLine();
-												// 129.97.7.120
-		Socket socket = new Socket(serverAddress, PORT);
+//      System.out.println("Enter the IP address of a machine running the capitalize server:");
+      String serverAddress = "192.168.232.1";//new Scanner(System.in).nextLine();
+      										//129.97.7.120 
+      Socket socket = new Socket(serverAddress, PORT);
+      
+      //initialize server-client communication 
+//      BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+//      PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+      DataInputStream in = new DataInputStream(socket.getInputStream());
+	  DataOutputStream out = new DataOutputStream(socket.getOutputStream());
 
-		// initialize server-client communication
-		BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-		PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-
-		// this will display the welcome message that was sent from the server
-		System.out.println(in.readLine());
-
-		// my idea; create an initial GUI asking for IP and Port to form a
-		// connection, and then a separate GUI
-		// for user inputs. That way, if the port# and IP addresses don't match,
-		// connection will fail
+     //this will display the welcome message that was sent from the server
+//     System.out.println(in.readLine());
+	  System.out.println(in.readUTF());
+	  Scanner scanner = new Scanner(System.in);
+     while (true) {
+    	 //whatever is in this loop was just a test to read commands 
+    	 out.writeUTF("POST 2 3 10 20 white Meeting next friday fam");
+    	 String input = in.readUTF();
+//    	 System.out.println(input);
+    	 if (input.equals("")) {
+    		 break;
+    	 }
+    	 
+    	 out.writeUTF("GET color=white refersTo=boy");
+    	 String input2 = in.readUTF();
+    	 System.out.println(input2);
+    	 if (input2.equals("")) {
+    		 break;
+    	 }
+    	 
+     }
+     
+     socket.close();
 	}
 }
